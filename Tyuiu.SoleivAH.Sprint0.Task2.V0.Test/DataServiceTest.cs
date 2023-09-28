1e66f4e690f76a81dc8efcb296e92020f15a4928
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-09-28 15:41:24 +0500

    Task2

---
+++ Tyuiu.SoleivAH.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SoleivAH.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SoleivAH.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Алишка";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Алишка", res);
+        }
+    }
+}
